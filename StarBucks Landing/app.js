75f348c403d1e7e1f2ed9d83e30131cb60e3bc8d
ggsingla <ggsingla777@gmail.com>
2021-11-06 00:06:58 +0530

    Semantic HTML & Responsiveness Added

--- StarBucks Landing/app.js
+++ StarBucks Landing/app.js
@@ -1,10 +1,30 @@
-let toggleMode = document.querySelector(".toggleMode");
-let body = document.querySelector("body");
+/////////////////////////////////////////////
+// Dark Mode Toggle
+/////////////////////////////////////////////
+
+let toggleMode = document.querySelector('.toggleMode');
+let body = document.querySelector('body');
 
 toggleMode.onclick = function () {
-  console.log("fine this here 1");
-  toggleMode.classList.toggle("active");
-  body.classList.toggle("dark");
+  toggleMode.classList.toggle('active');
+  body.classList.toggle('dark');
 };
 
-console.log("code success");
+/////////////////////////////////////////////
+// Responsive NavBar Code
+/////////////////////////////////////////////
+
+var burger = document.getElementById('burger-hider');
+var navItems = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navItems.classList.toggle('active');
+  burger.classList.toggle('active');
+});
+main.addEventListener('click', () => {
+  if (main.classList == 'active') {
+    navItems.classList.remove('active');
+    burger.classList.remove('active');
+    main.classList.remove('active');
+  }
+});
