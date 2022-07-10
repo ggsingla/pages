44e5d2224c1390c36a64aaa665fbd274bc6e05a8
Gautam Singla <ggsingla777@gmail.com>
2022-07-10 12:15:55 +0530

    HTML Completed

--- Agency Page/app.js
+++ Agency Page/app.js
@@ -1,1 +1,35 @@
 console.log('Hi 👋')
+const clients = document.getElementById('clients')
+const team = document.getElementById('team')
+
+// Team Data
+const teamNames = {
+  Vadim: 'Managing Director',
+  Sergery: 'Technology Director',
+  Andrey: 'Front-end Developer',
+  Alex: 'Front-end Developer',
+  Agneshka: 'Mobile Developer',
+  Dmitry: 'Back-end Developer',
+  Artyom: 'Back-end Developer',
+  Evgeny: 'Product Designer',
+  Tom: 'Product Designer',
+}
+
+// Populating the Clients section
+for (let i = 1; i <= 12; i++) {
+  const client = document.createElement('img')
+  client.src = `./Assets/Clients/client${i}.svg`
+  client.alt = `Client ${i}`
+  clients.appendChild(client)
+}
+let i = 1
+for (const key in teamNames) {
+  if (Object.hasOwnProperty.call(teamNames, key)) {
+    const el = teamNames[key]
+    const member = document.createElement('div')
+    member.classList.add('team-member')
+    member.innerHTML = `<img src="./Assets/Avatars/Avatar${i}.png" alt="Employee ${i}"><div class="member-data"><h4 class="member-name">${key}</h4><p class="member-designation">${el}</p></div>`
+    team.appendChild(member)
+    i++
+  }
+}
